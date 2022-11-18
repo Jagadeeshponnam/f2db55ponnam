d31b6ee9466d6c489d61158b0631a5de8c73db08
Jagadeeshponnam <112121032+Jagadeeshponnam@users.noreply.github.com>
2022-11-18 11:35:51 -0600

    adding passport

--- app.js
+++ app.js
@@ -20,9 +20,26 @@ var gridbuildRouter = require('./routes/gridbuild');
 var selectorRouter = require('./routes/selector');
 var electricity = require("./models/electricity");
 var resourceRouter = require("./routes/resource");
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username }, function (err, user) {
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  });
+  }
+ ));
 
 var app = express();
-
+  
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -31,6 +48,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -39,6 +65,7 @@ app.use('/electricity', electricityRouter);
 app.use('/gridbuild', gridbuildRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
+
 
 //Get the default connection
 var db = mongoose.connection;
@@ -77,12 +104,21 @@ console.log("Third object saved")
 let reseed = true;
 if (reseed) { recreateDB();}
 
-
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
+  
 });
+
+
 
 // error handler
 app.use(function(err, req, res, next) {
