90fe74b690bf537661577a9f9ab9bb76cf7ebaa6
Jagadeeshponnam <112121032+Jagadeeshponnam@users.noreply.github.com>
2022-11-30 23:52:07 -0600

    Bonus updated

--- models/electricity.js
+++ models/electricity.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const electricitySchema = mongoose.Schema({
-Edistributor_Name: String,
+Edistributor_Name:{type: String, minlength:1, maxlength:20},
 electricity_gen: {type: Number, min:1000, max:10000},
-elctricity_type: String
+elctricity_type: {type: String, minlength:1, maxlength:20},
 })
 module.exports = mongoose.model("electricity",
 electricitySchema)
